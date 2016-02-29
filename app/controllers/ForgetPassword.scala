9c3233251b8a357e91c9efd2721dcf88365a8997
PKOfficial <prabhatkashyap33@gmail.com>
2016-02-29 16:22:35 +0530

    SignUp Complete Login Complete ForgetPassword Complete Fully

--- app/controllers/ForgetPassword.scala
+++ app/controllers/ForgetPassword.scala
@@ -1,9 +1,14 @@
 package controllers
 
-import models.CustomerService
+import models.{CustomerService, Customer}
 import play.api.data.Form
 import play.api.data.Forms._
-import play.api.mvc.{Action, Controller}
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.i18n.Messages.Implicits._
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import play.api.Play.current
+import play.api.mvc.{ResponseHeader, Result, Controller, Action}
 
 /**
   * Created by akash on 29/2/16.
@@ -17,19 +22,21 @@ class ForgetPassword extends Controller{
     )
   }
 
-  def showForm = Action{
-    Ok("Here Show Forget")
+  def showForm = Action{ implicit request =>
+    Ok(views.html.forgetpass(customerForm))
   }
 
   def processForm = Action{ implicit request =>
 
     customerForm.bindFromRequest.fold(
 
-      formErrors => { Redirect(routes.SignUp.showForm())},
+      formErrors => {
+        BadRequest(views.html.forgetpass(formErrors))
+      },
       customerData => {
       val customerexist = customerObj.getCustomer(customerData)
         if(customerData != customerexist.email)
-          Redirect(routes.SignUp.showForm())
+          Redirect(routes.ForgetPassword.showForm()).flashing("error"->"Invalid Email")
         else
           Ok("Go to Login")
       })
